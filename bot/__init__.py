640f0463fb43e23ef6e834d0b1721442b6c1df86
erkutorenk <52276842+erkutorenk@users.noreply.github.com>
2021-05-26 17:00:54 +0300

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -8,7 +8,7 @@ if os.path.exists('config.env'):
 api_id = int(os.environ.get("API_ID"))
 api_hash = os.environ.get("API_HASH")
 bot_token = os.environ.get("BOT_TOKEN")
-download_dir = os.environ.get("DOWNLOAD_DIR", "indirilenler/")
+download_dir = os.environ.get("DOWNLOAD_DIR", "downloads/")
 sudo_users = list(set(int(x) for x in os.environ.get("SUDO_USERS").split()))
 
 app = Client(":memory:", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
